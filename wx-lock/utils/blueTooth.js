5af8799db6b935318d21a716cfde226bddce0247
tianliuzhen <tlzallen@163.com>
2021-08-06 22:14:58 +0800

    fix蓝牙开门

--- wx-lock/utils/blueTooth.js
+++ wx-lock/utils/blueTooth.js
@@ -227,6 +227,9 @@ function sendBLECharacterNotice(pointer) {
       })
       // 规定：头部+53，尾部+0D
       wrireToBlueToothDevice("53" + res.data.data + "0D" + "\\n")
+
+      // 发送完数据之后，断开连接
+      closeBlueTooth(pointer)
     }
   })
 }
