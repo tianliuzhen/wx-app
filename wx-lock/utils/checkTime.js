9580b16f0c5f01cc1de72ae34af140446245bf75
tianliuzhen <tlzallen@163.com>
2021-08-13 20:41:04 +0800

    css样式美化

--- wx-lock/utils/checkTime.js
+++ wx-lock/utils/checkTime.js
@@ -1,8 +1,11 @@
 
 // 检查时间
 function  verifyTime(startTime,endTime){
+ 
   var beginTime=startTime.date +"-"+startTime.time
   var endTime=endTime.date +"-"+endTime.time
+  console.log((beginTime.replace(/-/g,"").replace(":","")))
+  console.log(endTime.replace(/-/g,"").replace(":",""));
   if(beginTime.replace(/-/g,"").replace(":","") >= endTime.replace(/-/g,"").replace(":","")){
     wx.showToast({
       title: '有效开始时间不能大于等于有效结束时间！',
@@ -12,6 +15,12 @@ function  verifyTime(startTime,endTime){
     return false;
   }
   return true
+}
+function   buO(time) {
+  if (time >= 1 && time <= 9) {
+    time = "0" + time;
+  }
+  return time
 }
 
 module.exports = {
